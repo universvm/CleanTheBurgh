5bca6968c367632a216298b7f8bad609c8aa5d0f
universvm <leonardo.castorina98@gmail.com>
2018-03-10 20:18:20 +0000

    add diff algorithms

---
+++ newshackathon/main.py
@@ -0,0 +1,58 @@
+import numpy as np
+from collections import Counter
+import json
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from newshackathon.dataloading.data_constructor import construct_data_set
+
+def split_data(data_array):
+    """"""
+
+    X = data_array[:, :-1]
+    Y = data_array[:, -1]
+
+    return train_test_split(X, Y, train_size=0.8, random_state=42)
+
+def train_gnb():
+    gnb = GaussianNB()
+    gnb.fit(X_train, Y_train)
+
+    return accuracy_score(Y_test, gnb.predict(X_test))
+
+def train_forest():
+    rf = RandomForestClassifier(max_features=200, max_depth=300)
+    rf.fit(X_train, Y_train)
+
+    return accuracy_score(Y_test, rf.predict(X_test))
+
+
+def train_svc_poly():
+    svc_poly = SVC(kernel="poly").fit(X_train, Y_train)
+
+    return svc_poly.score(X_test, Y_test)
+
+
+def train_svc_linear():
+    svc_linear = SVC(kernel="linear").fit(X_train, Y_train)
+
+    return svc_linear.score(X_test, Y_test)
+
+
+def train_svc_rbf():
+    svc_rbf = SVC(kernel="rbf").fit(X_train, Y_train)
+
+    return svc_rbf.score(X_test, Y_test)
+
+data = construct_data_set()
+
+X_train, X_test, Y_train, Y_test = split_data(data)
+
+train_svc_linear()
