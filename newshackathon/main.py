11ead823ef190195940f3948156602d798baab2a
Kacper Kielak <kielak.kacper@gmail.com>
2018-03-11 00:55:39 +0000

    Add more stats prining and fix main

--- newshackathon/main.py
+++ newshackathon/main.py
@@ -15,35 +15,39 @@ def split_data(data_array):
 def train_gnb():
     gnb = GaussianNB()
     gnb.fit(X_train, Y_train)
-    return accuracy_score(Y_test, gnb.predict(X_test))
+    return accuracy_score(Y_test, gnb.predict(X_test)), accuracy_score(Y_train, gnb.predict(X_train))
 
 
 def train_forest():
     rf = RandomForestClassifier(max_features=200, max_depth=300)
     rf.fit(X_train, Y_train)
-    return accuracy_score(Y_test, rf.predict(X_test))
+    return accuracy_score(Y_test, rf.predict(X_test)), accuracy_score(Y_train, rf.predict(X_train))
 
 
 def train_svc_poly():
-    svc_poly = SVC(kernel="poly").fit(X_train, Y_train)
-    return svc_poly.score(X_test, Y_test)
+    svc_poly = SVC(kernel="poly")
+    svc_poly.fit(X_train, Y_train)
+    return accuracy_score(Y_test, svc_poly.predict(X_test)), accuracy_score(Y_train, svc_poly.predict(X_train))
 
 
 def train_svc_linear():
-    svc_linear = SVC(kernel="linear").fit(X_train, Y_train)
-    return svc_linear.score(X_test, Y_test)
+    svc_linear = SVC(kernel="linear")
+    svc_linear.fit(X_train, Y_train)
+    return accuracy_score(Y_test, svc_linear.predict(X_test)), accuracy_score(Y_train, svc_linear.predict(X_train))
 
 
 def train_svc_rbf():
-    svc_rbf = SVC(kernel="rbf").fit(X_train, Y_train)
-    return svc_rbf.score(X_test, Y_test)
+    svc_rbf = SVC(kernel="rbf")
+    svc_rbf.fit(X_train, Y_train)
+    return accuracy_score(Y_test, svc_rbf.predict(X_test)), accuracy_score(Y_train, svc_rbf.predict(X_train))
 
 data = construct_data_set()
+print('dataset size: {}'.format(len(data)))
 
 X_train, X_test, Y_train, Y_test = split_data(data)
 
-print('Gaussian NB accuracy: ' + str(train_forest()))
-print('Random Forest accuracy: ' + str(train_forest()))
-print('SVM Polly accuracy: ' + str(train_svc_poly()))
-print('SVM Linear accuracy: ' + str(train_svc_linear()))
-print('SVM RBF accuracy: ' + str(train_svc_rbf()))
+print('Gaussian NB accuracy (test, train): ' + str(train_forest()))
+print('Random Forest accuracy (test, train): ' + str(train_forest()))
+print('SVM Polly accuracy (test, train): ' + str(train_svc_poly()))
+print('SVM Linear accuracy (test, train): ' + str(train_svc_linear()))
+print('SVM RBF accuracy (test, train): ' + str(train_svc_rbf()))
